ce140d5cc37ef31c6ccb8908ebac6ec0b48ef911
Willian Justen <willianjustenqui@gmail.com>
2016-03-27 12:20:19 -0300

    Fix script

--- ci_script.sh
+++ ci_script.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-for file in topics/*.md
-do
-    bundle exec awesome_bot $file --allow-redirect --set-timeout 10
-done
+bundle exec awesome_bot Readme.md --allow-redirect --set-timeout 10
