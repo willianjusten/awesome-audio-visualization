b25a7f73b2ecb90cb3bc26757af25e841e54d525
Willian Justen <willianjustenqui@gmail.com>
2017-12-16 13:37:08 -0200

    Fix script and add .gitignore

--- ci_script.sh
+++ ci_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec awesome_bot Readme.md --allow-redirect --allow-ssl --set-timeout 10 --white-list soniaboller.github.io processing http://codecall.net/ https://github.com/sindresorhus/awesome, https://www.processing.org, https://tympanus.net/Development/MusicalInteractions, https://www.uberviz.io, https://airtightinteractive.com
+bundle exec awesome_bot Readme.md --allow-redirect --allow-ssl --set-timeout 10 --white-list soniaboller.github.io,processing,http://codecall.net/,https://github.com/sindresorhus/awesome,https://www.processing.org,https://tympanus.net/Development/MusicalInteractions,https://www.uberviz.io,https://airtightinteractive.com
